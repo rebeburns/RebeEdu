d50a37cc7136f6545451b7ffa539e7ef52014b2b
Rebecca <rebe.burns@gmail.com>
2013-02-06 21:39:14 +0100

    Change

--- Prueba/src/githubtest/Test.java
+++ Prueba/src/githubtest/Test.java
@@ -2,11 +2,8 @@ package githubtest;
 
 public class Test {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Hello edu");
 
 	}
 
