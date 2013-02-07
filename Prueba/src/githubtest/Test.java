4711f52e543b50c59983da1d735d1f9d0eca2729
Rebecca <rebe.burns@gmail.com>
2013-02-07 18:15:16 +0100

    Branching

--- Prueba/src/githubtest/Test.java
+++ Prueba/src/githubtest/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 	System.out.println("Hello edu");
 	System.out.println("Why aren´t you here?");
+	System.out.println("New branch?");
 	}
 
 }
