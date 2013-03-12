149b7bb110ecae8c0e8c5d5b7c04aaf4cdb7397b
Sergio <Sergio@Alumno11>
2013-03-12 20:13:46 +0100

    segundos

--- Prueba/src/githubtest/Test.java
+++ Prueba/src/githubtest/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 	System.out.println("Hello edu");
 	System.out.println("Hi!");
+	System.out.println("probando modificar sin estar en modo Git");
 	}
 
 }
